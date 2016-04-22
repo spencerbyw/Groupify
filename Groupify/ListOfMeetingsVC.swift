7b826bc10bacd861445bf68acfd4ec28f8abd2f9
spencerbyw <spencer.bywater@gmail.com>
2016-04-22 13:14:51 -0700

    Added a picker view to choose place, updates map on select.

--- Groupify/ListOfMeetingsVC.swift
+++ Groupify/ListOfMeetingsVC.swift
@@ -20,6 +20,8 @@ class ListOfMeetingsVC: UIViewController, UITableViewDelegate, UITableViewDataSo
     
     var namesDic = [String:String]()
     var placeDic = [String:String]()
+    var places_arr = [String]()
+    var places_set = Set<String>()
 
     @IBOutlet weak var meetings_table: UITableView!
     
@@ -45,19 +47,22 @@ class ListOfMeetingsVC: UIViewController, UITableViewDelegate, UITableViewDataSo
             let formatter = NSDateFormatter()
             formatter.dateStyle = NSDateFormatterStyle.LongStyle
             formatter.timeStyle = .ShortStyle
-            print(each.meeting_availability_start)
+//            print(each.meeting_availability_start)
             let time_available = String(formatter.stringFromDate(each.meeting_availability_start!))
-            print(time_available)
+//            print(time_available)
             
             if !timesAvailable.contains(time_available) {
                 timesAvailable.append(time_available)
                 namesDic[String(time_available)] = each.name
                 placeDic[String(time_available)] = each.preferred_meeting_location
-                
+                places_set.insert(each.preferred_meeting_location!)
+//                places_arr.append(each.preferred_meeting_location!)
             } else {
                 //else add the name to the list of members available
                 namesDic[String(time_available)] = namesDic[String(time_available)]! + " , " + each.name!
                 placeDic[String(time_available)] = placeDic[String(time_available)]! + " : " + each.preferred_meeting_location!
+//                places_arr.append(each.preferred_meeting_location!)
+                places_set.insert(each.preferred_meeting_location!)
             }
         }
         
@@ -91,6 +96,7 @@ class ListOfMeetingsVC: UIViewController, UITableViewDelegate, UITableViewDataSo
                 viewController.groupName = selectedGroup?.name
                 viewController.locations_available = placeDic[timesAvailable[selectedIndex.row]]
                 viewController.members_available = namesDic[timesAvailable[selectedIndex.row]]
+                viewController.place_list = Array(places_set)
             }
         }
     }
--- Groupify/ScheduleMeetingDetailsVC.swift
+++ Groupify/ScheduleMeetingDetailsVC.swift
@@ -9,21 +9,25 @@
 import UIKit
 import CoreData
 import Foundation
+import MapKit
 
-class ScheduleMeetingDetailsVC: UIViewController {
+class ScheduleMeetingDetailsVC: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
     
     var groupName: String?
     var date_time: String?
     var members_available: String?
     var locations_available: String?
+    var place_list: [String]?
+    var selected_place: String?
     
     var context = (UIApplication.sharedApplication().delegate as! AppDelegate).managedObjectContext
 
     
+    @IBOutlet weak var mapBox: MKMapView!
+    @IBOutlet weak var placePicker: UIPickerView!
     @IBOutlet weak var groupNameLabel: UILabel!
     @IBOutlet weak var metingDateTimeLabel: UILabel!
     @IBOutlet weak var availableMemberBox: UITextView!
-    @IBOutlet weak var locationsBox: UITextView!
     
     @IBAction func Schedule_Meeting(sender: AnyObject) {
                 let context = self.context
@@ -35,7 +39,7 @@ class ScheduleMeetingDetailsVC: UIViewController {
                 nItem.date_time = date_time
                 nItem.group_name = groupName
                 nItem.members_available = members_available
-                nItem.places = locations_available
+                nItem.places = selected_place
         
                 do {
                     try context.save()
@@ -48,17 +52,56 @@ class ScheduleMeetingDetailsVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        if !(place_list?.isEmpty)! {
+            getDirections(place_list![0])
+        }
+        placePicker.dataSource = self
+        placePicker.delegate = self
+        print(place_list)
 
         groupNameLabel.text = groupName
         metingDateTimeLabel.text = date_time
         availableMemberBox.text = members_available
-        locationsBox.text = locations_available
         // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return place_list!.count
+    }
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return place_list![row]
+    }
+    
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        selected_place = place_list![row]
+        getDirections(selected_place!)
+    }
+    
+    func getDirections(sender: AnyObject) {
+        let address = ( sender as! NSString)
+        
+        let geocoder = CLGeocoder()
+        geocoder.geocodeAddressString (address as String, completionHandler: {(placemarks: [CLPlacemark]?, error: NSError?) -> Void in
+            if let placemark = placemarks?[0]  {
+                let span = MKCoordinateSpanMake(0.1, 0.1)
+                let region = MKCoordinateRegion(center: placemark.location!.coordinate, span: span)
+                self.mapBox.setRegion(region, animated: true)
+                let ani = MKPointAnnotation()
+                ani.coordinate = placemark.location!.coordinate
+                ani.title = address as String
+                
+                self.mapBox.addAnnotation(ani)
+            }
+        })
     }
 
 }
